df2e2b09344be10d75f281809e3e1f4f507d3661
Daniel Digney <dedigney@gmail.com>
2017-08-30 18:26:46 -0500

    Adjust users contoller to registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
+    @user = User.new(allowed_params)
     if @user.save
-      session[:id] = @user.id
+      # session[:id] = @user.id
       redirect_to root_path, notice: "You are now logged in as #{@user.first_name} #{@user.last_name}."
     else
-      render 'new'
+      # render 'new'
+      redirect_to root_path
     end
   end
 
   private
 
   def allowed_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :mentor)
   end
 end
