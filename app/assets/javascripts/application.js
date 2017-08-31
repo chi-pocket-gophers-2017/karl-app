92c6b63ae983408d613005983f0d04549a5892c0
dandanthedevman <danielkrzyzanowskiphoto@gmail.com>
2017-08-31 15:15:56 -0500

    removed turbo links fixed jquery issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require rails-ujs
-//= require turbolinks
-//= require_tree .
 //= require jquery-3.2.1.min
 //= require materialize
 //= require init
-
+//= require_tree .
