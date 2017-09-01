30b8717c61f98c54bfd6e472f4e06fc15d5ae2cd
Dillon Forestell <djlf729@gmail.com>
2017-09-01 15:08:46 -0500

    config production env for redis on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.web_socket_server_url = "wss://karl-app.herokuapp.com/cable"
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.action_cable.allowed_request_origins = ['https://karl-app.herokuapp.com', 'http://karl-app.herokuapp.com']
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
