d70ce4bdd69eda25989847b0c06323fbf153a16a
dandanthedevman <danielkrzyzanowskiphoto@gmail.com>
2017-09-01 11:40:20 -0500

    schema is updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831164409) do
+ActiveRecord::Schema.define(version: 20170901162024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170831164409) do
     t.boolean "mentor", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_url"
   end
 
 end
