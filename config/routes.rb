8cb3b4ecf8b651ef4602df2c00dde76d88eb6ca5
dandanthedevman <danielkrzyzanowskiphoto@gmail.com>
2017-09-01 14:18:18 -0500

    added route to edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   root 'welcome#index'
   get '/queue', to: "users#show"
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
   resources :mentors, controller: 'users', only: [:new], action: "new_mentor"
   resources :mentors, controller: 'users', only: [:create], action: "create_mentor"
   resources :students, controller: 'users', only: [:new, :create]
